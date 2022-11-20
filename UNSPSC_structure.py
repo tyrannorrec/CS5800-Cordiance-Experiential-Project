b7526354ebf2c7c606459e2553846325ee7b9940
tyrannorrec <92607420+tyrannorrec@users.noreply.github.com>
2022-11-20 14:15:38 -0800

    UNSPSC_structure file init
    UNSPSC_structure file init commit
    data_parser file indent fix

---
+++ UNSPSC_structure.py
@@ -0,0 +1,164 @@
+import pandas as pd
+import time
+
+# ------------------------------------------------------------------------------------------------#
+
+# Object represents tree structure holding all UNSPSC data.
+# Hierarchy: UNSPSC_Tree -> UNSPSC_Segment -> UNSPSC_Family -> UNSPSC_Class -> UNSPSC_Commodity
+# Attributes: df          -- Pandas data frame containing information read from excel file
+#             segmentList -- list of UNSPSC_Segment objects in tree
+class UNSPSC_Tree:
+
+    # Class initializer. Builds tree and prints time taken to build.
+    def __init__(self, inDataFrame):
+
+        self.df = inDataFrame
+        self.segmentList = []
+
+        print('>> Building UNSPSC tree...')
+        start = time.time()
+        self.build()
+        print('>>>> Build complete. Process took %s seconds.\n' % (round((time.time() - start), 2)))
+
+    # Parses data frame and builds tree. Called by initializer.
+    def build(self):
+
+        # Takes str param and returns list of words in str in lower caps.
+        def getWordList(inStr):
+
+            if isinstance(inStr, str) and len(inStr) > 0:
+                return inStr.lower().split(' ')
+            else:
+                return []
+
+        currSegmentID, currFamilyID, currClassID = 0, 0, 0
+        currSegment, currFamily, currClass = None, None, None
+
+        for i in self.df.index: # for every line in df
+
+            if self.df['Segment'][i] != currSegmentID: # if new segment ID, create new segment
+                currSegment = UNSPSC_Segment(self.df['Segment'][i], 
+                getWordList(self.df['Segment Title'][i]), 
+                getWordList(self.df['Segment Definition'][i]))
+                self.segmentList.append(currSegment) # append to UNSPSC tree's segment list
+                currSegmentID = self.df['Segment'][i] # update curr segment ID
+
+            else: # if segment already exists
+
+                if self.df['Family'][i] != currFamilyID: # if new family ID, create new family
+                    currFamily = UNSPSC_Family(self.df['Family'][i], 
+                    getWordList(self.df['Family Title'][i]), 
+                    getWordList(self.df['Family Definition'][i]))
+                    currSegment.familyList.append(currFamily) # append to curr segment's family list
+                    currFamilyID = self.df['Family'][i] # update curr family ID
+
+                else: # if family already exists
+                    
+                    if self.df['Class'][i] != currClassID: # if new class ID, create new class
+                        currClass = UNSPSC_Class(self.df['Class'][i],
+                        getWordList(self.df['Class Title'][i]), 
+                        getWordList(self.df['Class Definition'][i]))
+                        currFamily.classList.append(currClass) # append to curr family's class list
+                        currClassID = self.df['Class'][i] # update curr class ID
+
+                    else: # if class already exists
+
+                        currCommodity = UNSPSC_Commodity(self.df['Key'][i], self.df['Commodity'][i],
+                        getWordList(self.df['Commodity Title'][i]), 
+                        getWordList(self.df['Definition'][i]))
+                        currClass.commodityList.append(currCommodity) # append to curr class's commodity list
+
+    # Print function for testing.
+    def printAll(self):
+
+        for seg in self.segmentList:
+            print(seg.id, ' ', seg.title, '')
+            for fam in seg.familyList:
+                print('\t', fam.id, ' ', fam.title)
+                for cla in fam.classList:
+                    print('\t\t', cla.id, '', cla.title)
+                    for com in cla.commodityList:
+                        print('\t\t\t', com.id, com.title)
+
+# ------------------------------------------------------------------------------------------------#
+
+# Object represents Segment in UNSPSC tree structure.
+# Hierarchy: UNSPSC_Segment -> UNSPSC_Family -> UNSPSC_Class -> UNSPSC_Commodity
+# Attributes: id          -- ID number of Segment (int)
+#             title       -- list of words in name of Segment (list of str)
+#             definition  -- list of words in description of Segment (list of str)
+#             familyList  -- list of UNSPSC_Segment objects in tree
+class UNSPSC_Segment:
+
+    def __init__(self, inID, inTitle, inDef):
+        self.id = int(inID)
+        self.title = inTitle
+        self.definition = inDef
+        self.familyList = []
+
+# ------------------------------------------------------------------------------------------------#
+
+# Object represents Family in UNSPSC tree structure.
+# Hierarchy: UNSPSC_Family -> UNSPSC_Class -> UNSPSC_Commodity
+# Attributes: id          -- ID number of Family (int)
+#             title       -- list of words in name of Family (list of str)
+#             definition  -- list of words in description of Family (list of str)
+#             classList   -- list of UNSPSC_Class objects in Segment
+class UNSPSC_Family:
+
+    def __init__(self, inID, inTitle, inDef):
+        self.id = int(inID)
+        self.title = inTitle
+        self.definition = inDef
+        self.classList = []
+
+# ------------------------------------------------------------------------------------------------#
+
+# Object represents Class in UNSPSC tree structure.
+# Hierarchy: UNSPSC_Class -> UNSPSC_Commodity
+# Attributes: id          -- ID number of Class (int)
+#             title       -- list of words in name of Class (list of str)
+#             definition  -- list of words in description of Class (list of str)
+#             commodityList -- list of UNSPSC_Commodity objects in Class
+class UNSPSC_Class:
+
+    def __init__(self, inID, inTitle, inDef):
+        self.id = int(inID)
+        self.title = inTitle
+        self.definition = inDef
+        self.commodityList = []
+
+# ------------------------------------------------------------------------------------------------#
+
+# Object represents Class in UNSPSC tree structure.
+# Attributes: key         -- unique key of item (int)
+#             id          -- ID number of Class (int)
+#             title       -- list of words in name of Commodity (list of str)
+#             definition  -- list of words in description of Commodity (list of str)
+class UNSPSC_Commodity:
+
+    def __init__(self, inKey, inID, inTitle, inDef):
+        self.key = int(inKey)
+        self.id = int(inID)
+        self.title = inTitle
+        self.definition = inDef
+
+# ------------------------------------------------------------------------------------------------#
+
+# Reads in UNSPSC excel file, converts to Pandas data frame, and returns a UNSPSC_Tree object.
+def getTree():
+
+    print('\n>> Converting UNSPSC file to data frame...')
+    start = time.time()
+    UNSPSC_df = pd.read_excel('UNSPSC_English_excel.xlsx', 'UNSPSC_English_excel', skiprows=9)
+    print('>>>> Conversion complete. Process took %s seconds.\n' % (round((time.time() - start), 2)))
+
+    return UNSPSC_Tree(UNSPSC_df)
+
+# ------------------------------------------------------------------------------------------------#
+
+def main():
+    UNSPSC_tree = getTree()
+
+if __name__ == "__main__":
+    main()
--- data_parser.py
+++ data_parser.py
@@ -1,67 +1,67 @@
-    import numpy as np
-    import pandas as pd
+import numpy as np
+import pandas as pd
 
 
-    def avalara_parse():
-        # read in file
-        df_ava = pd.read_excel('Avalara_goods_and_services.xlsx', 
-                        'goods_and_services',
-                        header=1)
+def avalara_parse():
+    # read in file
+    df_ava = pd.read_excel('Avalara_goods_and_services.xlsx', 
+                    'goods_and_services',
+                    header=1)
+
+    # printing out column names                
+    column_names = list(df_ava.columns.str.replace(' ', ''))
+    # print(column_names)
+
+    # set option to display all rows
+    pd.set_option('display.max_rows', None)
+    # print(df.to_string())
+
+    # filtering the parent node (larger category type) ending by '0000'
+    suffix = ['000000']
+
+
+    filtered_col = df_ava[df_ava['AvaTax System Tax Code'].str.endswith(tuple(suffix), na=False)]
+    print(filtered_col) 
+
+
+def UNSPSC_parse():
+    df_UNSPSC = pd.read_excel('UNSPSC_English_excel.xlsx', 
+                    'UNSPSC_English_excel',
+                    skiprows=9)
+
+    # printing out column names                
+    # column_names = list(df_UNSPSC.columns.str.replace(' ', ''))
+    # print(column_names)
+
+    count = 0
+    segment_title = []
+
+    # Going through all segment title and add the distinct ones in to the segment_tile list
+    for i in range(0, len(df_UNSPSC['Segment Title'])):
+        str = df_UNSPSC['Segment Title'][i]
+        if str not in segment_title:
+            segment_title.append(str)
+            count += 1
+        else:
+            continue
     
-        # printing out column names                
-        column_names = list(df_ava.columns.str.replace(' ', ''))
-        # print(column_names)
+    # in case we want to resize the segment_title to represent in a matrix format
+    # ending = 10 - count % 10
+    # for i in range(0, ending):
+    #     segment_title.append("null")
 
-        # set option to display all rows
-        pd.set_option('display.max_rows', None)
-        # print(df.to_string())
-
-        # filtering the parent node (larger category type) ending by '0000'
-        suffix = ['000000']
-    
-
-        filtered_col = df_ava[df_ava['AvaTax System Tax Code'].str.endswith(tuple(suffix), na=False)]
-        print(filtered_col) 
+    # rows = 10
+    # cols = int (len(segment_title) / 10)
+    # org_segment_title = np.reshape(segment_title, (rows, cols))
+    # print(org_segment_title)
+    print(segment_title)
+    print(f"The total number of distinct segment titles are: {count}")
 
 
-    def UNSPSC_parse():
-        df_UNSPSC = pd.read_excel('UNSPSC_English_excel.xlsx', 
-                        'UNSPSC_English_excel',
-                        skiprows=9)
-
-        # printing out column names                
-        # column_names = list(df_UNSPSC.columns.str.replace(' ', ''))
-        # print(column_names)
-
-        count = 0
-        segment_title = []
-
-        # Going through all segment title and add the distinct ones in to the segment_tile list
-        for i in range(0, len(df_UNSPSC['Segment Title'])):
-            str = df_UNSPSC['Segment Title'][i]
-            if str not in segment_title:
-                segment_title.append(str)
-                count += 1
-            else:
-                continue
-            
-        # in case we want to resize the segment_title to represent in a matrix format
-        # ending = 10 - count % 10
-        # for i in range(0, ending):
-        #     segment_title.append("null")
-
-        # rows = 10
-        # cols = int (len(segment_title) / 10)
-        # org_segment_title = np.reshape(segment_title, (rows, cols))
-        # print(org_segment_title)
-        print(segment_title)
-        print(f"The total number of distinct segment titles are: {count}")
+def main():
+    # avalara_parse()
+    UNSPSC_parse()
 
 
-    def main():
-        # avalara_parse()
-        UNSPSC_parse()
-
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
